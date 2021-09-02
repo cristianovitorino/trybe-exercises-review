f7bfab70ef082f8a4544b6553d98461caf58658a
Cristiano Vitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-09-02 04:06:00 -0300

    Adiciona exercício 2

---
+++ bloco-8/higher-order-functions-reduce/exercise2.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Crie uma string com os nomes de todas as pessoas autoras.
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames() {
+  const names = books.reduce((acc, book, index, array) => // reduce recebe um acumulador, o valor atual e o índice do array
+    index === array.length - 1 ? // Se for o último elemento do array
+      `${acc} ${book.author.name}.` : // Retorna o nome do autor
+      `${acc} ${book.author.name},`, // Se não for (ou enquanto não for), concatena o nome do autor com vírgula
+    ''); // Inicia a string com vazio
+  return names.trim(); // Retorna a string sem espaços no início e no fim
+}
+
+assert.strictEqual(reduceNames(), expectedResult);
